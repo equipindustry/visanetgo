fd4ce3c377899ce21b46a6151f4ccb9f74030bd3
Cristhoper Alvarez <acristhoper25@gmail.com>
2020-07-13 15:03:06 -0500

    refactor: modify client http(#4)

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -1,59 +1,20 @@
 package client
 
 import (
-	"encoding/base64"
-	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
 
 	"github.com/equipindustry/visanetgo/pkg/api/status"
-	"github.com/spf13/viper"
 )
 
-// Authorization returns response and error.
-func Authorization() ([]byte, error) {
-	url := viper.GetString("VISANET_DEV_AUTHORIZATION_API")
-	req, err := http.NewRequest("POST", url, nil)
-	if err != nil {
-		return nil, err
-	}
-	user := viper.Get("VISANET_DEV_USER")
-	pwd := viper.Get("VISANET_DEV_PASSWD")
-	encodedCredential := base64.StdEncoding.EncodeToString([]byte(fmt.Sprintf("%s:%s", user, pwd)))
-	req.Header.Set("Authorization", fmt.Sprintf("Basic %s", encodedCredential))
-
-	c := &http.Client{}
-	res, err := c.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	defer res.Body.Close()
-
-	obj, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	if err != nil {
-		err = fmt.Errorf("%v: %s", err, string(obj))
-		return nil, err
-	}
-
-	if res.StatusCode == status.Created {
-		return obj, nil
-	}
-
-	return nil, err
-}
-
 // Client returns a response byte.
-func Client(method string, url string, body io.Reader, token string) ([]byte, error) {
+func Client(method string, url string, body io.Reader, headers http.Header) ([]byte, error) {
 	req, err := http.NewRequest(method, url, body)
 	if err != nil {
 		return nil, err
 	}
-	req.Header.Set("Authorization", token)
+	req.Header = headers
 
 	c := &http.Client{}
 	res, err := c.Do(req)
@@ -67,14 +28,9 @@ func Client(method string, url string, body io.Reader, token string) ([]byte, er
 		return nil, err
 	}
 
-	if err != nil {
-		err = fmt.Errorf("%v: %s", err, string(obj))
-		return nil, err
-	}
-
-	if res.StatusCode == status.Ok {
+	if res.StatusCode >= status.Ok && res.StatusCode <= status.Accepted {
 		return obj, nil
 	}
 
-	return nil, err
+	return obj, err
 }
