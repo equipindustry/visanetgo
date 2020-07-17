331941795f21c8b69838812b13f73ba4e1c95209
Cristhoper Alvarez <acristhoper25@gmail.com>
2020-07-16 22:12:53 -0500

    refactor: adding session body and response and instance credentials (#14)

--- pkg/session/session.go
+++ pkg/session/session.go
@@ -6,15 +6,44 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/equipindustry/visanetgo/model"
+	"github.com/equipindustry/visanetgo/pkg/visanet"
+	"github.com/equipindustry/visanetgo/utils"
+
 	"github.com/equipindustry/visanetgo/pkg/client"
-	"github.com/spf13/viper"
 )
 
+// Session struct.
+type Session struct {
+	Amount              float64   `json:"amount"`
+	Antifraud           Antifraud `json:"antifraud"`
+	Channel             string    `json:"channel"`
+	RecurrenceMaxAmount *float64  `json:"recurrenceMaxAmount"`
+}
+
+// Antifraud struct.
+type Antifraud struct {
+	ClientIP           string             `json:"clientIp"`
+	MerchantDefineData MerchantDefineData `json:"merchantDefineData"`
+}
+
+// MerchantDefineData struct.
+type MerchantDefineData struct {
+	Mdd4  string `json:"MDD4"`
+	Mdd32 string `json:"MDD32"`
+	Mdd75 string `json:"MDD75"`
+	Mdd77 int    `json:"MDD77"`
+}
+
+// ResponseSession struct.
+type ResponseSession struct {
+	SessionKey     string
+	ExpirationTime int
+}
+
 // CreateSession returns struct and string.
-func CreateSession(token string, sessionBody model.SessionBody) (*model.SessionResponse, error) {
-	url := fmt.Sprintf("%s/%s", viper.GetString("VISANET_DEV_SESSION_API"), viper.GetString("VISANET_DEV_MERCHANT_ID"))
-	sessionResponse := model.SessionResponse{}
+func (s *Session) CreateSession(token string) (*ResponseSession, error) {
+	url := fmt.Sprintf(utils.GetSessionAPIURL(visanet.Credentials.IsDevelopment), visanet.Credentials.MerchantID)
+	sessionResponse := ResponseSession{}
 
 	header := http.Header{
 		"Authorization": {
@@ -25,7 +54,7 @@ func CreateSession(token string, sessionBody model.SessionBody) (*model.SessionR
 		},
 	}
 
-	byteArray, err := json.Marshal(sessionBody)
+	byteArray, err := json.Marshal(s)
 	if err != nil {
 		fmt.Println(err)
 	}
