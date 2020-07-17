3b1aa7c44d7417e753651bfb1127d8bf07a7ba26
Cristhoper Alvarez <acristhoper25@gmail.com>
2020-07-16 22:03:05 -0500

    refactor: add error for status code (#14)

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -1,11 +1,19 @@
 package client
 
 import (
+	"errors"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
 
 	"github.com/equipindustry/visanetgo/pkg/api/status"
+)
+
+var (
+	errInvalidRequest = errors.New("The request has invalid syntax")
+	errAuthentication = errors.New("The request could not be processed due to problems with the keys")
+	errUnexpected     = errors.New("Unexpected error, response code is not handled")
 )
 
 // Client returns a response byte.
@@ -28,9 +36,21 @@ func Client(method string, url string, body io.Reader, headers http.Header) ([]b
 		return nil, err
 	}
 
+	switch res.StatusCode {
+	case status.Unauthorized:
+		err = errAuthentication
+	case status.BadRequest:
+		err = errInvalidRequest
+	}
+
+	if err != nil {
+		err = fmt.Errorf("%v: %s", err, string(obj))
+		return nil, err
+	}
+
 	if res.StatusCode >= status.Ok && res.StatusCode <= status.Accepted {
 		return obj, nil
 	}
 
-	return obj, err
+	return nil, errUnexpected
 }
